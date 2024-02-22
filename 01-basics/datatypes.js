5a8130ed9eafd1849fbfd129d56f4484b975b0a9
Nikhil Kant Pandey <110042418+nikhilpandey1994@users.noreply.github.com>
2024-02-22 19:29:42 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,9 @@
+// string ="", number =12 , boolean = true/false , null =standalone value, bigInt,undefined, symbol
+//object
+// typeof - show that which type of datatype 
+let name ="nikhil"
+let age = 12
+let isLoggedIn= false
+console.log (typeof "nikhil")
+console.log(typeof null);
+console.log(typeof undefined);
