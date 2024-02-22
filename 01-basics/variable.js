e7552ddf89f408d44a5ba2b7d38c8ecee0531d3a
Nikhil Kant Pandey <110042418+nikhilpandey1994@users.noreply.github.com>
2024-02-22 19:12:21 +0000

    container  config done

---
+++ 01-basics/variable.js
@@ -0,0 +1,10 @@
+const accountId =14453
+let accountEmail="knt1994007@gmail.com"
+console.log(accountId);
+// change value
+/*
+   not use var because of issue in block and function scope.
+*/
+accountEmail ="ksdhfkjfd@gmail.com"
+accountPassword="12346"
+console.table([accountEmail,accountId,accountPassword])
